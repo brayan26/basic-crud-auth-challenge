7d8a394128c1070caad35a5ef5bef4d582c34162
bparra26 <brayanpp2697@gmail.com>
2024-05-16 23:13:32 -0500

    feat: i add adapters

---
+++ src/context/shared/infrastructure/adapters/jsonplaceholder/ExternalApi.ts
@@ -0,0 +1,21 @@
+import config from '@app/config';
+import AxiosClientFactory from '@context/shared/infrastructure/clients/AxiosClientFactory';
+import { HTTP_VERBS } from '@context/shared/infrastructure/constants/HttpVerbs';
+import User from '@context/users/domain/class/User';
+import { AxiosResponse } from 'axios';
+
+export default class ExternalApi extends AxiosClientFactory {
+  private headers: any = {
+    'Content-Type': 'application/json',
+  };
+  private BASE_URL: string = config.CRUD_API;
+  private CREATE_USER_PATH: string = `${this.BASE_URL}/users`;
+
+  async create(user: User): Promise<AxiosResponse<any>> {
+    return this.invoke(this.CREATE_USER_PATH, HTTP_VERBS.POST, this.headers, user);
+  }
+
+  async getAll(): Promise<AxiosResponse<any>> {
+    return this.invoke(this.CREATE_USER_PATH, HTTP_VERBS.GET, this.headers);
+  }
+}
